e5fc0a85d67d8ea29098ebcb8e0a05f505766633
Radek Roža <roza@ws24.cz>
2018-07-06 14:43:50 +0200

    Created Static version of Story box

---
+++ Box/Story.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roza
+ * Date: 06/07/2018
+ * Time: 12:06
+ */
+
+namespace Wbengine\Box;
+
+
+class Article
+{
+
+}
---
+++ Box/Story/StoryModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roza
+ * Date: 06/07/2018
+ * Time: 13:03
+ */
+
+class Model
+{
+
+}
